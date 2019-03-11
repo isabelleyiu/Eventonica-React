f9ec6299adf3a3ae38feed9a20df7e6fdb04bf46
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-03-11 11:39:43 -0700

    Use bootstrap style instead of css

--- client/eventonica-react/src/Event.js
+++ client/eventonica-react/src/Event.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import Button from 'react-bootstrap/Button';
+import { Button, Card } from 'react-bootstrap';
 import Moment from 'react-moment';
-import './Event.css'
+// import './Event.css'
 
 class Event extends Component {
   constructor(props){
@@ -12,15 +12,20 @@ class Event extends Component {
     const {title, start_time, venue_name, venue_address, selectedEvent, deleteEvent} = this.props;
 
     return (
-      <div className="event-card">
-        <h3>{title}</h3>
-        <h4>Date: <Moment format="dddd, MMMM Do YYYY">{start_time}</Moment></h4>
-        <h4>Time: <Moment format="h:mma">{start_time}</Moment></h4>
-        <h4>Venue: {venue_name}</h4>
-        <h4>Address: {venue_address}</h4>
-        <Button variant="outline-danger" onClick={selectedEvent}>EDIT</Button> 
-        <Button variant="outline-danger" onClick={deleteEvent}>DELETE</Button> 
-      </div>
+      <Card bg="light" style={{ width: '18rem' ,margin: '10px 10px'}}>
+        <Card.Header>Suggested Events</Card.Header>
+        <Card.Body>
+          <h3>{title}</h3>
+          <Card.Text>
+          <h6>Date: <Moment format="dddd, MMMM Do YYYY">{start_time}</Moment></h6>
+          <h6>Time: <Moment format="h:mma">{start_time}</Moment></h6>
+          <h6>Venue: {venue_name}</h6>
+          <h6>Address: {venue_address}</h6>
+          </Card.Text>
+          <Button variant="outline-danger" onClick={selectedEvent} style={{marginRight: '5px'}}>EDIT</Button> 
+          <Button variant="danger" onClick={deleteEvent}>DELETE</Button> 
+        </Card.Body>
+      </Card>
     )
   }
 }

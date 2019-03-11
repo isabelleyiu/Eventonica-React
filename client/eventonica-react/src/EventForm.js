441bd18d5f4577eb20f3061a62d5a9672d55895d
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-03-11 11:41:14 -0700

    Use bootstrap for EventForm styling

--- client/eventonica-react/src/EventForm.js
+++ client/eventonica-react/src/EventForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './EventInput.css'
+import { Button, Form, Row, Col } from 'react-bootstrap';
 import moment from 'moment';
 
 class EventForm extends Component {
@@ -65,22 +65,55 @@ class EventForm extends Component {
   render() {
     const {title, date, time, venue_name, venue_address} = this.state;
     return (
-      <div className="event-form">
-        <form>
-          <h2>Create New Event</h2>
-          <label>EVENT</label>
-          <input type="text" placeholder="Learn React" name="title" value={title} onChange={this.handleChange}></input>
-          <label>DATE</label>
-          <input type="text" placeholder="YYYY-MM-DD" name="date" value={date} onChange={this.handleChange}></input>
-          <label>TIME</label>
-          <input type="text" placeholder="06:30 pm" name="time" value={time} onChange={this.handleChange}></input>
-          <label>VENUE</label>
-          <input type="text" placeholder="Techtonica" name="venue_name" value={venue_name} onChange={this.handleChange}></input>
-          <label>ADDRESS</label>
-          <input type="text" placeholder="201 Mission Street" name="venue_address" value={venue_address} onChange={this.handleChange}></input>
-          <button onClick={this.handleSubmit}>Submit</button>
-        </form>
-      </div>
+      // <div className="event-form">
+      //   <Form>
+      //     <h2>Create New Event</h2>
+      //     <label>EVENT</label>
+      //     <input type="text" placeholder="Learn React" name="title" value={title} onChange={this.handleChange}></input>
+      //     <label>DATE</label>
+      //     <input type="text" placeholder="YYYY-MM-DD" name="date" value={date} onChange={this.handleChange}></input>
+      //     <label>TIME</label>
+      //     <input type="text" placeholder="06:30 pm" name="time" value={time} onChange={this.handleChange}></input>
+      //     <label>VENUE</label>
+      //     <input type="text" placeholder="Techtonica" name="venue_name" value={venue_name} onChange={this.handleChange}></input>
+      //     <label>ADDRESS</label>
+      //     <input type="text" placeholder="201 Mission Street" name="venue_address" value={venue_address} onChange={this.handleChange}></input>
+      //     <button onClick={this.handleSubmit}>Submit</button>
+      //   </Form>
+      // </div>
+<Form >
+  <Form.Group>
+    <Form.Label style={{color: 'white'}}>EVENT</Form.Label>
+    <Form.Control type="text" placeholder="Learn React" name="title" value={title} onChange={this.handleChange} />
+  </Form.Group>
+  <Form.Row>
+    <Form.Group as={Col}>
+      <Form.Label style={{color: 'white'}}>DATE</Form.Label>
+      <Form.Control type="text" placeholder="YYYY-MM-DD"  name="date" value={date} onChange={this.handleChange} />
+    </Form.Group>
+
+    <Form.Group as={Col}>
+      <Form.Label style={{color: 'white'}}>TIME</Form.Label>
+      <Form.Control type="text" placeholder="06:30 pm" name="time" value={time} onChange={this.handleChange} />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Row>
+    <Form.Group as={Col}>
+      <Form.Label style={{color: 'white'}}>VENUE</Form.Label>
+      <Form.Control placeholder="Techtonica" name="venue_name" value={venue_name} onChange={this.handleChange} />
+    </Form.Group>
+
+    <Form.Group as={Col}>
+      <Form.Label style={{color: 'white'}}>Address</Form.Label>
+      <Form.Control placeholder="1234 Main St" name="venue_address" value={venue_address} onChange={this.handleChange} />
+    </Form.Group>
+  </Form.Row>
+
+  <Button variant="warning" type="submit" onClick={this.handleSubmit}>
+    Submit
+  </Button>
+</Form>
     )
   }
 }

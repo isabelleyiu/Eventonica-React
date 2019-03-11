9657a448a114809c644ac15cb27f771ec138c77a
Isabelle Yiu <isabelle.yiu.jobs@gmail.com>
2019-03-10 20:57:08 -0700

    clean up code

--- client/eventonica-react/src/Event.js
+++ client/eventonica-react/src/Event.js
@@ -8,15 +8,16 @@ class Event extends Component {
   }
     
   render() {
-    const {title, start_time, venue_name, venue_address} = this.props
+    const {title, start_time, venue_name, venue_address, selectedEvent, deleteEvent} = this.props;
+
     return (
       <div className="event-card">
-        <h3>{this.props.title}</h3>
-        <h4>Time: {this.props.start_time}</h4>
-        <h4>Venue: {this.props.venue_name}</h4>
-        <h4>Address: {this.props.venue_address}</h4>
-        <Button variant="outline-danger" onClick={this.props.selectedEvent}>EDIT</Button> 
-        <Button variant="outline-danger" onClick={this.props.deleteEvent}>DELETE</Button> 
+        <h3>{title}</h3>
+        <h4>Time: {start_time}</h4>
+        <h4>Venue: {venue_name}</h4>
+        <h4>Address: {venue_address}</h4>
+        <Button variant="outline-danger" onClick={selectedEvent}>EDIT</Button> 
+        <Button variant="outline-danger" onClick={deleteEvent}>DELETE</Button> 
       </div>
     )
   }
